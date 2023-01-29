5efeebc4527dbf10f4e574c88dde67134bb2aa3e
Zu33air <zu33air@gmail.com>
2023-01-30 01:27:17 +0500

    first commit

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Todo from "./todo";
+
+export default function Page() {
+  return (
+    <div>
+      <Todo />
+    </div>
+  );
+}
---
+++ app/todo.js
@@ -0,0 +1,82 @@
+"use client";
+import React, { useState } from "react";
+
+export default function Todo() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const onClickHandler = (myElm) => {
+    console.log("meraElm", myElm);
+
+    // map runs on array, and returns an array
+    const newTodos = todos.map((todo) => {
+      console.log("todo: ", todo);
+      if (todo.todoText == myElm.todoText) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+
+    console.log(newTodos);
+    setTodos(newTodos);
+  };
+  const addTodo = () => {
+    const newTodo = { todoText: todo, completed: false };
+    const newTodos = [...todos, newTodo];
+    setTodos(newTodos);
+    setTodo("");
+  };
+
+  const deleteTodo = (meraTodo) => {
+    const newTodos = todos.filter((todo) => {
+      if (todo.todoText == meraTodo.todoText) return false;
+      return true;
+    });
+    console.log("old todos", todos, "new todos", newTodos);
+    setTodos(newTodos);
+  };
+  return (
+    <>
+      <div>Todo</div>
+      <input
+        placeholder="add todo text"
+        value={todo}
+        onChange={(e) => {
+          setTodo(e.target.value);
+        }}
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((elm) => {
+          return (
+            <li
+              style={{
+                color: elm.completed ? "green" : "orange",
+                fontStyle: "oblique",
+                listStyle: "none",
+              }}
+              key={elm.todoText}
+            >
+              <input
+                type="checkbox"
+                checked={elm.completed}
+                onChange={() => {
+                  onClickHandler(elm);
+                }}
+              />
+              {elm.todoText}
+              <button
+                onClick={() => {
+                  deleteTodo(elm);
+                }}
+              >
+                {"  "}
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
